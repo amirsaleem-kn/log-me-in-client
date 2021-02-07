b39dff1440aaba97bbbdc2450a6d146b3a250e5f
Amir Saleem <developer.amirsaleem@gmail.com>
2021-02-08 00:40:24 +0530

    fix: headers error

--- src/util/request.js
+++ src/util/request.js
@@ -3,15 +3,11 @@ class Request {
 
     static init(options) {
         options.baseURL = "https://amirsaleem.in";
-        if (options.headers) {
-            options.headers = {
-                "Content-Type": "application/json",
-                "x-api-key": "6dbb951682c7c81cc30a06fc311227be",
-            }
-        } else {
-            options.headers["Content-Type"] = "application/json";
-            options.headers["x-api-key"] = "6dbb951682c7c81cc30a06fc311227be";
+        if (options.headers == null) {
+            options.headers = {};
         }
+        options.headers["Content-Type"] = "application/json";
+        options.headers["x-api-key"] = "6dbb951682c7c81cc30a06fc311227be";
         return axios(options);
 
     }
