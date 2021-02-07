472bf1a2c9e561f8b93a2a332291962c72f0db71
Amir Saleem <developer.amirsaleem@gmail.com>
2021-02-08 00:14:42 +0530

    add backend details

--- src/util/request.js
+++ src/util/request.js
@@ -3,15 +3,15 @@ class Request {
 
     static init(options) {
 
-        options.baseUrl = process.env.APIURL;
+        options.baseUrl = `https://amirsaleem.in`; // This should come from .env file
         if (options.headers) {
             options.headers = {
                 "Content-Type": "application/json",
-                "x-api-key": process.env.APIKEY,
+                "x-api-key": `6dbb951682c7c81cc30a06fc311227be`, // this should not be exposed to version controler, due to less time, doing this.
             }
         } else {
             options.headers["Content-Type"] = "application/json";
-            options.headers["x-api-key"] = process.env.APIKEY;
+            options.headers["x-api-key"] = `6dbb951682c7c81cc30a06fc311227be`;
         }
         return axios(options);
 
